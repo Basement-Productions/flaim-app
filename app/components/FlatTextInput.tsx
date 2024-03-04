f1ae5f4efbd4ec05c68bc00412e4165c4272a334
amanpuranik <56619430+amanpuranik@users.noreply.github.com>
2024-03-03 19:44:34 -0500

    implement comments ui

--- app/components/FlatTextInput.tsx
+++ app/components/FlatTextInput.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { TextInput, TextInputProps, useTheme } from 'react-native-paper'
 
-export default function FlatTextInput(props: TextInputProps) {
+export default function FlatTextInput({ marginTop = 10, ...props }: { marginTop?: number } & TextInputProps) {
     let clr = useTheme().colors;
     return (
         <TextInput
             {...props}
-            className="w-full mt-10"
-            style={{ backgroundColor: clr.background }}
+            className="w-full"
+            style={{ marginTop, backgroundColor: clr.background }}
             mode="flat"
             outlineColor={clr.primary}
             outlineStyle={{ borderRadius: 10, borderWidth: 0 }}
-        />)
+        />
+    );
 }
+
 
